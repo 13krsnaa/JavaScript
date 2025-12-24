23e03b02f515dc4218a88dad995b450992aed6c0
13krsnaa <saxenakrishna9140@gmail.com>
2025-12-24 17:02:37 +0530

    Completed task-1

---
+++ phase-1-fundamentals/task-1/index.js
@@ -0,0 +1,23 @@
+// I'll have to declare the variables name and print them with their types
+// Type String =>
+const name = "Keshav Saxena";
+console.log("Name:", name);
+console.log("Type :", typeof name);
+ // Type Number =>
+const age = 19;
+ console.log("Age:", age);
+ console.log("type", typeof age);
+//Type boolean 
+const isStudent = true ;
+ console.log("isStudent :", isStudent);
+ console.log("Type:", typeof isStudent);
+// Type Undefined 
+let score;
+console.log("Score :", score);
+console.log("Type :", typeof score);
+// Type Null 
+ const nullVar = null;
+console.log("Null Value:", nullVar);
+console.log("Null Type:", typeof nullVar);
+console.log("---");
+
