ce7203f5ac91e6484bf1810223d9fe22c74a093b
13krsnaa <saxenakrishna9140@gmail.com>
2025-12-24 20:33:57 +0530

    completed task-2

---
+++ phase-1-fundamentals/task-2/index.js
@@ -0,0 +1,30 @@
+// We Will make a simple CLI-calculator 
+// here we will take 2 input and a operator , the code will be hard coded for now
+
+// Select any 2 numbers 
+const input1 = 10;
+const input2 = 4;
+
+// Select the operator fron + , - , * , /
+const operator ='*';
+ 
+// Use if else for all the conditions 
+if(operator === '+'){
+    console.log(input1 + input2);
+ } else if(operator === '-'){
+    console.log(input1 - input2);
+ } else if(operator === '*'){
+    console.log(input1 * input2);
+ } 
+ //make sure to handle the division by zero case
+ 
+ else if(operator === '/'){
+     if (input2 !== 0){
+        console.log(input1 / input2);
+     }
+        else if(input2 === 0){
+            console.log("The number cannot be divided by Zero");
+        }
+     }
+
+ 
